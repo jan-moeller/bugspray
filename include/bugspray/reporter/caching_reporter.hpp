46e26f156075163f1fbe704c0ac74610d9d2daa3
Jan Möller <drag-on@gmx.de>
2022-04-10 17:55:51 +0200

    Implement caching_reporter
    This reporter will come in handy for testing.

---
+++ include/bugspray/reporter/caching_reporter.hpp
@@ -0,0 +1,158 @@
+//
+// MIT License
+//
+// Copyright (c) 2022 Jan Möller
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#ifndef BUGSPRAY_CACHING_REPORTER_HPP
+#define BUGSPRAY_CACHING_REPORTER_HPP
+
+#include "bugspray/reporter/reporter.hpp"
+#include "bugspray/utility/source_location.hpp"
+#include "bugspray/utility/string.hpp"
+#include "bugspray/utility/vector.hpp"
+
+#include <algorithm>
+#include <span>
+#include <string_view>
+
+/*
+ * This reporter simply caches all data for later use.
+ */
+
+namespace bs
+{
+struct caching_reporter : reporter
+{
+    struct assertion_data
+    {
+        std::string_view       text;
+        source_location        sloc;
+        bs::vector<bs::string> messages;
+        bool                   result;
+    };
+    struct section_data;
+    struct assertion_and_section_holder
+    {
+        bs::vector<assertion_data> assertions;
+        bs::vector<section_data>   sections;
+    };
+    struct section_data : assertion_and_section_holder
+    {
+        std::string_view name;
+        source_location  sloc;
+    };
+    struct test_run_data : assertion_and_section_holder
+    {
+        section_path target;
+    };
+    struct test_case_data
+    {
+        std::string_view                  name;
+        std::span<std::string_view const> tags;
+        source_location                   sloc;
+
+        bs::vector<test_run_data> test_runs;
+    };
+
+#if defined(__GNUC__) && !defined(__clang__) && !defined(__INTEL_COMPILER)
+    // TODO: check if gcc bug https://gcc.gnu.org/bugzilla/show_bug.cgi?id=93413 is fixed
+    constexpr ~caching_reporter(){};
+#endif
+
+    constexpr void enter_test_case(std::string_view                  name,
+                                   std::span<std::string_view const> tags,
+                                   source_location                   sloc) noexcept override
+    {
+        m_test_cases.emplace_back(name, tags, sloc, bs::vector<test_run_data>{});
+    }
+
+    constexpr void leave_test_case() noexcept override
+    {
+    }
+
+    constexpr void start_run(section_path const& target) noexcept override
+    {
+        m_test_cases.back().test_runs.push_back({{}, target});
+    }
+
+    constexpr void stop_run() noexcept override
+    {
+    }
+
+    constexpr void enter_section(std::string_view name, source_location sloc) noexcept override
+    {
+        create_section(name, sloc);
+        m_current_section.push_back(name);
+    }
+
+    constexpr void leave_section() noexcept override
+    {
+        m_current_section.pop();
+    }
+
+    constexpr void log_assertion(std::string_view            assertion,
+                                 source_location             sloc,
+                                 std::span<bs::string const> messages,
+                                 bool                        result) noexcept override
+    {
+        auto*                  s = find_section(m_current_section);
+        bs::vector<bs::string> msgs{messages.begin(), messages.end()};
+        s->assertions.emplace_back(assertion, sloc, msgs, result);
+    }
+
+    constexpr void finalize() noexcept override
+    {
+    }
+
+    [[nodiscard]] constexpr auto cache() const noexcept -> bs::vector<test_case_data> const&
+    {
+        return m_test_cases;
+    }
+
+  private:
+    constexpr auto find_section(section_path const& path) noexcept -> assertion_and_section_holder*
+    {
+        auto&                         cur_test_case = m_test_cases.back();
+        assertion_and_section_holder* cur_section   = &cur_test_case.test_runs.back();
+        for (std::size_t i = 0; i < path.size(); ++i)
+        {
+            auto const s    = path[i];
+            auto*      iter = std::ranges::find_if(cur_section->sections,
+                                              [&s](section_data& subsection) { return subsection.name == s; });
+            assert(iter != cur_section->sections.end());
+            cur_section = iter;
+        }
+        return cur_section;
+    }
+
+    constexpr void create_section(std::string_view name, source_location const& sloc) noexcept
+    {
+        assertion_and_section_holder* cur = find_section(m_current_section);
+        cur->sections.push_back({{}, name, sloc});
+    }
+
+    bs::vector<test_case_data> m_test_cases;
+    section_path               m_current_section;
+};
+} // namespace bs
+
+#endif // BUGSPRAY_CACHING_REPORTER_HPP
---
+++ test/unit_tests/reporter/test_caching_reporter.cpp
@@ -0,0 +1,119 @@
+//
+// MIT License
+//
+// Copyright (c) 2022 Jan Möller
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+#include "bugspray/reporter/caching_reporter.hpp"
+
+#include <catch2/catch_all.hpp>
+
+using namespace bs;
+
+TEST_CASE("caching_reporter", "[reporter]")
+{
+    constexpr std::string_view filename = "some_file.cpp";
+
+    constexpr std::string_view test_case_name = "test_case";
+    constexpr std::string_view test_case_tag  = "tag";
+    constexpr std::size_t      test_case_line = 10;
+
+    constexpr std::string_view section_name = "section";
+    constexpr std::size_t      section_line = 20;
+
+    constexpr std::string_view assertion         = "FAIL()";
+    constexpr std::size_t      assertion_line    = 30;
+    constexpr std::string_view assertion_message = "WITH something super important";
+    constexpr bool             assertion_result  = false;
+
+    constexpr auto test = [filename,
+                           test_case_name,
+                           test_case_tag,
+                           test_case_line,
+                           section_name,
+                           section_line,
+                           assertion,
+                           assertion_line,
+                           assertion_message,
+                           assertion_result]()
+    {
+        caching_reporter reporter;
+
+        bs::vector<std::string_view> tags{};
+        // TODO: check if gcc bug https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100495 has been fixed
+        if (!std::is_constant_evaluated()) // This works around above bug
+            tags.push_back(test_case_tag);
+        reporter.enter_test_case(test_case_name, tags, source_location{.file_name = filename, .line = test_case_line});
+
+        reporter.start_run(section_path{});
+        reporter.stop_run();
+
+        reporter.start_run(section_path{section_name});
+        reporter.enter_section(section_name, source_location{.file_name = filename, .line = section_line});
+
+        bs::vector<bs::string> messages{};
+        // TODO: check if gcc bug https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100495 has been fixed
+        if (!std::is_constant_evaluated()) // This works around above bug
+            messages.push_back(assertion_message);
+        reporter.log_assertion(assertion,
+                               source_location{.file_name = filename, .line = assertion_line},
+                               messages,
+                               assertion_result);
+
+        reporter.leave_section();
+        reporter.stop_run();
+        reporter.leave_test_case();
+
+        return reporter.cache();
+    };
+
+#define MAKE_TESTS(PREFIX)                                                                                             \
+    PREFIX##REQUIRE(test().size() == 1);                                                                               \
+    PREFIX##REQUIRE(test()[0].name == test_case_name);                                                                 \
+    PREFIX##REQUIRE(test()[0].sloc.file_name == filename);                                                             \
+    PREFIX##REQUIRE(test()[0].sloc.line == test_case_line);                                                            \
+    PREFIX##REQUIRE(test()[0].test_runs.size() == 2);                                                                  \
+    PREFIX##REQUIRE(test()[0].test_runs[0].target == section_path{});                                                  \
+    PREFIX##REQUIRE(test()[0].test_runs[0].assertions.size() == 0);                                                    \
+    PREFIX##REQUIRE(test()[0].test_runs[0].sections.size() == 0);                                                      \
+    PREFIX##REQUIRE(test()[0].test_runs[1].target == section_path{section_name});                                      \
+    PREFIX##REQUIRE(test()[0].test_runs[1].assertions.size() == 0);                                                    \
+    PREFIX##REQUIRE(test()[0].test_runs[1].sections.size() == 1);                                                      \
+    PREFIX##REQUIRE(test()[0].test_runs[1].sections[0].name == section_name);                                          \
+    PREFIX##REQUIRE(test()[0].test_runs[1].sections[0].sloc.file_name == filename);                                    \
+    PREFIX##REQUIRE(test()[0].test_runs[1].sections[0].sloc.line == section_line);                                     \
+    PREFIX##REQUIRE(test()[0].test_runs[1].sections[0].sections.size() == 0);                                          \
+    PREFIX##REQUIRE(test()[0].test_runs[1].sections[0].assertions.size() == 1);                                        \
+    PREFIX##REQUIRE(test()[0].test_runs[1].sections[0].assertions[0].text == assertion);                               \
+    PREFIX##REQUIRE(test()[0].test_runs[1].sections[0].assertions[0].sloc.file_name == filename);                      \
+    PREFIX##REQUIRE(test()[0].test_runs[1].sections[0].assertions[0].sloc.line == assertion_line);                     \
+    PREFIX##REQUIRE(test()[0].test_runs[1].sections[0].assertions[0].result == assertion_result);
+
+    MAKE_TESTS(STATIC_)
+    MAKE_TESTS()
+
+    // TODO: The following tests are only made at runtime due to gcc bug 100495, see above.
+    REQUIRE(test()[0].tags.size() == 1);
+    REQUIRE(test()[0].tags[0] == test_case_tag);
+    REQUIRE(test()[0].test_runs[1].sections[0].assertions[0].messages.size() == 1);
+    REQUIRE(test()[0].test_runs[1].sections[0].assertions[0].messages[0] == assertion_message);
+
+#undef MAKE_TESTS
+}
