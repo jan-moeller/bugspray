f344a9a19022d62b3b691149a5a1310890df46ae
Jan Möller <drag-on@gmx.de>
2022-04-18 17:29:21 +0200

    Implement stringify(auto)

---
+++ include/bugspray/to_string/stringify.hpp
@@ -0,0 +1,51 @@
+//
+// MIT License
+//
+// Copyright (c) 2022 Jan Möller
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#ifndef BUGSPRAY_STRINGIFY_HPP
+#define BUGSPRAY_STRINGIFY_HPP
+
+#include "bugspray/to_string/to_string_bool.hpp"
+#include "bugspray/to_string/to_string_char.hpp"
+#include "bugspray/to_string/to_string_integral.hpp"
+#include "bugspray/to_string/to_string_string_like.hpp"
+#include "bugspray/to_string/to_string_tag.hpp"
+#include "bugspray/utility/string.hpp"
+
+namespace bs
+{
+template<typename T>
+constexpr auto stringify(T&& thing) -> bs::string
+{
+    if constexpr (requires { to_string(bs::to_string_override_tag{}, std::forward<T>(thing)); })
+        return to_string(bs::to_string_override_tag{}, std::forward<T>(thing));
+    else if constexpr (requires { to_string(std::forward<T>(thing)); })
+        return to_string(std::forward<T>(thing));
+    else if constexpr (requires { to_string(bs::to_string_tag{}, std::forward<T>(thing)); })
+        return to_string(bs::to_string_tag{}, std::forward<T>(thing));
+    else
+        return "<?>";
+}
+} // namespace bs
+
+#endif // BUGSPRAY_STRINGIFY_HPP
---
+++ test/unit_tests/to_string/test_stringify.cpp
@@ -0,0 +1,91 @@
+//
+// MIT License
+//
+// Copyright (c) 2022 Jan Möller
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+#include "bugspray/to_string/stringify.hpp"
+
+#include <catch2/catch_all.hpp>
+
+namespace ns
+{
+struct foo
+{
+};
+struct bar
+{
+};
+struct baz
+{
+};
+struct bam
+{
+};
+
+constexpr auto to_string(foo const&) -> bs::string
+{
+    return "adl";
+}
+
+constexpr auto to_string(bar const&) -> bs::string
+{
+    return "adl";
+}
+} // namespace ns
+
+namespace bs
+{
+constexpr auto to_string(bs::to_string_tag, ns::foo const&) -> bs::string
+{
+    return "bs";
+}
+constexpr auto to_string(bs::to_string_tag, ns::bar const&) -> bs::string
+{
+    return "bs";
+}
+constexpr auto to_string(bs::to_string_tag, std::same_as<ns::baz> auto&&) -> bs::string
+{
+    return "bs";
+}
+} // namespace bs
+
+namespace bs
+{
+constexpr auto to_string(to_string_override_tag, ns::foo const&) -> bs::string
+{
+    return "override";
+}
+} // namespace bs
+
+TEST_CASE("stringify", "[to_string]")
+{
+#define MAKE_TESTS(PREFIX)                                                                                             \
+    PREFIX##CHECK(bs::stringify(ns::foo{}) == "override");                                                             \
+    PREFIX##CHECK(bs::stringify(ns::bar{}) == "adl");                                                                  \
+    PREFIX##CHECK(bs::stringify(ns::baz{}) == "bs");                                                                   \
+    PREFIX##CHECK(bs::stringify(ns::bam{}) == "<?>");                                                                  \
+    PREFIX##CHECK(bs::stringify(true) == "true");                                                                      \
+    PREFIX##CHECK(bs::stringify('a') == R"('a')");
+
+    MAKE_TESTS()
+    MAKE_TESTS(STATIC_)
+#undef MAKE_TESTS
+}
