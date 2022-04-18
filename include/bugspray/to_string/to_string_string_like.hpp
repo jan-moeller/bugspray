a55cf2e333ea446d0cf10edef7cccda67cf39cd1
Jan Möller <drag-on@gmx.de>
2022-04-18 17:27:46 +0200

    Implement to_string(string-like)

---
+++ include/bugspray/to_string/to_string_string_like.hpp
@@ -0,0 +1,48 @@
+//
+// MIT License
+//
+// Copyright (c) 2022 Jan Möller
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#ifndef BUGSPRAY_TO_STRING_STRING_LIKE_HPP
+#define BUGSPRAY_TO_STRING_STRING_LIKE_HPP
+
+#include "bugspray/to_string/char_to_printable_string.hpp"
+#include "bugspray/utility/string.hpp"
+
+#include <ranges>
+
+namespace bs
+{
+template<std::ranges::range T>
+constexpr auto to_string(T&& s) -> bs::string
+    requires std::same_as<std::ranges::range_value_t<T>,
+                          char>
+{
+    bs::string result = "\"";
+    for (auto&& c : s)
+        result += char_to_printable_string(c);
+    result += "\"";
+    return result;
+}
+} // namespace bs
+
+#endif // BUGSPRAY_TO_STRING_STRING_LIKE_HPP
