a5b38b5b17d48eb654a4e008ff2ea068f6f8d6fc
Jan Möller <drag-on@gmx.de>
2022-04-30 16:08:47 +0200

    Improve stringify tests

--- test/unit_tests/to_string/test_stringify.cpp
+++ test/unit_tests/to_string/test_stringify.cpp
@@ -83,7 +83,11 @@ TEST_CASE("stringify", "[to_string]")
     PREFIX##CHECK(bs::stringify(ns::baz{}) == "bs");                                                                   \
     PREFIX##CHECK(bs::stringify(ns::bam{}) == "<?>");                                                                  \
     PREFIX##CHECK(bs::stringify(true) == "true");                                                                      \
-    PREFIX##CHECK(bs::stringify('a') == R"('a')");
+    PREFIX##CHECK(bs::stringify('a') == R"('a')");                                                                     \
+    PREFIX##CHECK(bs::stringify("foo") == R"("foo\0")");                                                               \
+    PREFIX##CHECK(bs::stringify(std::string_view{"foo"}) == R"("foo")");                                               \
+    PREFIX##CHECK(bs::stringify(bs::string{"foo"}) == R"("foo")");                                                     \
+    PREFIX##CHECK(bs::stringify(42) == R"(42)");
 
     MAKE_TESTS()
     MAKE_TESTS(STATIC_)
