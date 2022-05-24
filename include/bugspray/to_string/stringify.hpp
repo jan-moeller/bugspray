e9a495e366c4baa5adcde0c8ae1156496193de70
Jan Möller <drag-on@gmx.de>
2022-05-24 22:14:40 +0200

    Use operator<< for stringification if no better method available

--- include/bugspray/to_string/stringify.hpp
+++ include/bugspray/to_string/stringify.hpp
@@ -34,6 +34,8 @@
 #include "bugspray/to_string/to_string_tag.hpp"
 #include "bugspray/utility/string.hpp"
 
+#include <sstream>
+
 namespace bs
 {
 template<typename T>
@@ -45,8 +47,16 @@ constexpr auto stringify(T&& thing) -> bs::string
         return to_string(std::forward<T>(thing));
     else if constexpr (requires { to_string(bs::to_string_tag{}, std::forward<T>(thing)); })
         return to_string(bs::to_string_tag{}, std::forward<T>(thing));
-    else
-        return "<?>";
+    else if constexpr (requires { std::declval<std::stringstream>() << std::forward<T>(thing); })
+    {
+        if (!std::is_constant_evaluated())
+        {
+            std::stringstream ss;
+            ss << std::forward<T>(thing);
+            return bs::string{ss.str()};
+        }
+    }
+    return "<?>";
 }
 } // namespace bs
 
