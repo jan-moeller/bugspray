4f1d68df62313af90470df8ad9ae526430850f81
Jan Möller <drag-on@gmx.de>
2022-06-19 19:23:57 +0200

    Implement stringify_typename()

---
+++ include/bugspray/utility/stringify_typename.hpp
@@ -0,0 +1,95 @@
+//
+// MIT License
+//
+// Copyright (c) 2022 Jan Möller
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#ifndef BUGSPRAY_STRINGIFY_TYPENAME_HPP
+#define BUGSPRAY_STRINGIFY_TYPENAME_HPP
+
+#include <array>
+#include <string_view>
+#include <tuple>
+
+#include <cstddef>
+
+namespace bs
+{
+namespace detail
+{
+template<typename T>
+[[nodiscard]] constexpr auto pretty_function() -> std::string_view
+{
+#ifdef __clang__
+    return __PRETTY_FUNCTION__;
+#elif defined(__GNUC__)
+    return __PRETTY_FUNCTION__;
+#elif defined(_MSC_VER)
+    return __FUNCSIG__;
+#else
+    static_assert(false, "Unsupported compiler");
+#endif
+}
+
+struct typename_probe;
+struct pretty_function_metadata
+{
+    std::size_t prefix;
+    std::size_t suffix;
+};
+
+constexpr auto compute_pretty_function_metadata() -> pretty_function_metadata
+{
+    using namespace std::string_view_literals;
+    constexpr std::array options{"class bs::detail::typename_probe"sv,
+                                 "bs::detail::typename_probe"sv,
+                                 "typename_probe"sv};
+    constexpr auto       pretty_function_output = pretty_function<typename_probe>();
+    constexpr auto const result                 = [&]() -> std::pair<std::string_view, std::size_t>
+    {
+        for (auto&& opt : options)
+        {
+            auto const s = pretty_function_output.find(opt);
+            if (s != std::string_view::npos)
+                return std::make_pair(opt, s);
+        }
+        return std::make_pair("", -1);
+    }();
+    static_assert(result.second != std::string_view::npos,
+                  "This compiler formats __PRETTY_FUNCTION__ (or equivalent) in an unexpected way");
+    constexpr auto prefix = result.second;
+    constexpr auto suffix = pretty_function_output.size() - result.first.size() - prefix;
+    return pretty_function_metadata{prefix, suffix};
+}
+constexpr auto pretty_function_metadata = compute_pretty_function_metadata();
+} // namespace detail
+
+template<typename T>
+[[nodiscard]] constexpr auto stringify_typename() -> std::string_view
+{
+    using namespace detail;
+    constexpr auto name = pretty_function<T>();
+    constexpr auto meta = compute_pretty_function_metadata();
+    return name.substr(meta.prefix, name.size() - meta.prefix - meta.suffix);
+}
+} // namespace bs
+
+#endif // BUGSPRAY_STRINGIFY_TYPENAME_HPP
---
+++ test/unit_tests/utility/test_stringify_typename.cpp
@@ -0,0 +1,66 @@
+//
+// MIT License
+//
+// Copyright (c) 2022 Jan Möller
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+#include "bugspray/utility/stringify_typename.hpp"
+#include "bugspray/utility/structural_string.hpp"
+
+#include <catch2/catch_all.hpp>
+
+using namespace bs;
+
+namespace foobar
+{
+struct baz;
+}
+
+TEST_CASE("stringify_typename", "[utility]")
+{
+    STATIC_CHECK(stringify_typename<int>() == "int");
+    STATIC_CHECK(stringify_typename<int*>() == "int*");
+    STATIC_CHECK(stringify_typename<int[3]>() == "int [3]");
+    STATIC_CHECK(stringify_typename<const int>() == "const int");
+    STATIC_CHECK(stringify_typename<const int*>() == "const int*");
+    STATIC_CHECK(stringify_typename<const int&>() == "const int&");
+    STATIC_CHECK(stringify_typename<const int* const>() == "const int* const");
+    STATIC_CHECK(stringify_typename<const volatile int* const>() == "const volatile int* const");
+    STATIC_CHECK(stringify_typename<foobar::baz>() == "foobar::baz");
+    STATIC_CHECK(stringify_typename<std::vector<int>>() == "std::vector<int>");
+
+    constexpr auto lifetime_test = []
+    {
+        std::string_view name_of_int;
+        {
+            constexpr std::string_view typename_of_int = stringify_typename<int>();
+            name_of_int                                = typename_of_int;
+        }
+        return name_of_int;
+    };
+    STATIC_CHECK(lifetime_test() == "int");
+
+    constexpr auto create_structural_string_test = []
+    {
+        constexpr std::string_view sv = stringify_typename<int>();
+        return structural_string<sv.size()>{sv};
+    };
+    STATIC_CHECK(create_structural_string_test() == "int");
+}
